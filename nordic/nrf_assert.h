36d648ff382f383fc5f7d1976d5de186a24efc45
Nat Ersoz <natersoz@gmail.com>
2018-04-12 05:16:00 -0700

    Fix broken builds - oddly, this used to work
    Anyhow:
    Update README.md to note changed files in SDK
    Add stubborn nordic inclusions to nordic/nrf_assert.h

--- nordic/nrf_assert.h
+++ nordic/nrf_assert.h
@@ -5,5 +5,12 @@
 
 #pragma once
 
+// This is to satisfy many of the nordic files which include "nrf_assert.h"
+// to obtain "nrf.h", "app_error.h"
+#include <stdint.h>
+#include "nrf.h"
+#include "app_error.h"
+
+
 #include "project_assert.h"
 
