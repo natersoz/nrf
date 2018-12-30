3106c1291731e5789e033732ef04a27cea8b603f
Nat Ersoz <natersoz@gmail.com>
2018-12-30 11:27:21 -0800

    ble_peripheral/main.cc log level set to debug

--- ble_peripheral/main.cc
+++ ble_peripheral/main.cc
@@ -30,7 +30,7 @@ int main(void)
 
     logger& logger = logger::instance();
     logger.set_rtc(rtc_1);
-    logger.set_level(logger::level::info);
+    logger.set_level(logger::level::debug);
     logger.set_output_stream(rtt_os);
 
     logger.info("--- BLE peripheral ---");
